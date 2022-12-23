ca232430e5e869848d498f62bec281e75ef52123
Aashish Sharma <ashishchapain86@gmail.com>
2022-12-23 07:34:11 +0545

    completed CSLL

--- src/JavaLinkedlist/CSLL/DeleteCSLL.java
+++ src/JavaLinkedlist/CSLL/DeleteCSLL.java
@@ -1,5 +1,8 @@
 package JavaLinkedlist.CSLL;
 
+
+import java.util.Scanner;
+
 public class DeleteCSLL {
     class Node {
         int data;
@@ -39,11 +42,11 @@ public class DeleteCSLL {
         }
         else {
             Node temp = head;
-            while (temp.next.next != head) {
+            while (temp.next.next!= head) {
                 temp = temp.next;
             }
             temp.next = head;
-            temp.next.next = null;
+
         }
     }
 
@@ -75,6 +78,94 @@ public class DeleteCSLL {
                 temp = temp.next;
             }
             temp.next = temp.next.next;
+        }
+    }
+
+    public void  insertatBeg(int x){
+        Node newnode = new Node(x);
+        if(head==null){
+            head=newnode;
+            head.next=head;
+
+        }
+        else if (head.next==head){
+            head.next=newnode;
+            newnode.next=head;
+            head=newnode;
+        }
+        else {
+            Node temp = head;
+            while (temp.next!=head){
+                temp=temp.next;
+            }
+            temp.next=newnode;
+            newnode.next=head;
+            head=newnode;
+        }
+    }
+
+    public void display(){
+        Node temp = head;
+        if (head==null){
+            System.out.println("List is empty");
+        }
+        else {
+            do {
+                System.out.print(temp.data+" ");
+                temp = temp.next;
+            } while (temp!=head);
+            System.out.println();
+        }
+    }
+
+    public static void main(String args[])
+    {
+        DeleteCSLL s = new DeleteCSLL();
+        s.insertatBeg(34);
+        s.insertatBeg(99);
+        s.insertatBeg(677);
+
+        System.out.println("CSLL:");
+        s.display();
+
+        System.out.println("\nPress 1 for deletion at beginning");
+        System.out.println("Press 2 for deletion at end");
+        System.out.println("Press 3 for deletion at given position");
+        System.out.println("Enter your choice");
+
+        Scanner input=new Scanner(System.in);
+        int ch=input.nextInt();
+
+        switch(ch)
+        {
+            case 1:
+            {
+                s.deleteatBeg();
+                s.display();
+                break;
+            }
+            case 2:
+            {
+                s.deleteatLast();
+                s.display();
+                break;
+            }
+
+
+
+
+            case 3:
+            {
+                System.out.println("Enter the position of element");
+                int pos = input.nextInt();
+                s.deleteatPos(pos);
+                s.display();
+                break;
+            }
+            default:
+            {
+                System.out.println("Invalid Choice");
+            }
         }
     }
 }
--- src/JavaLinkedlist/CSLL/InsertCSLL.java
+++ src/JavaLinkedlist/CSLL/InsertCSLL.java
@@ -1,5 +1,8 @@
 package JavaLinkedlist.CSLL;
 
+
+import java.util.Scanner;
+
 public class InsertCSLL {
     class Node{
         int data;
@@ -12,13 +15,15 @@ public class InsertCSLL {
     }
     public Node head=null;
 
-    public void  inseratBeg(int x){
+
+    public void  insertatBeg(int x){
         Node newnode = new Node(x);
         if(head==null){
-            newnode.next=head;
             head=newnode;
+            head.next=head;
+
         }
-        if (head.next==head){
+        else if (head.next==head){
             head.next=newnode;
             newnode.next=head;
             head=newnode;
@@ -33,6 +38,26 @@ public class InsertCSLL {
             head=newnode;
         }
     }
+    public void  insertatEnd(int x){
+        Node newnode = new Node(x);
+        if(head==null){
+            head=newnode;
+            newnode.next=head;
+            head=newnode;
+        }
+        else if (head.next==head){
+            head.next=newnode;
+            newnode.next=head;
+        }
+        else {
+            Node temp = head;
+            while (temp.next!=head){
+                temp=temp.next;
+            }
+            temp.next=newnode;
+            newnode.next=head;
+        }
+    }
 
     public void insertatPos(int x,int pos){
         Node newnode = new Node(x);
@@ -41,9 +66,15 @@ public class InsertCSLL {
             System.out.println("Invalid position");
         }
         else if(pos==1){
-            if(head.next==head){
+            if(head==null){
+                head=newnode;
                 newnode.next=head;
+                head=newnode;
+            }
+
+            else if(head.next==head){
                 head.next=newnode;
+                newnode.next=head;
                 head=newnode;
             }
             else {
@@ -58,10 +89,10 @@ public class InsertCSLL {
         }
         else {
             Node temp = head;
-            for (int i = 1; i < pos-1&&temp!=head; i++) {
+            for (int i=1; i<pos-1&&temp.next!=head; i++) {
                 temp=temp.next;
             }
-            if (temp!=head){
+            if (temp.next!=head){
                 newnode.next=temp.next;
                 temp.next=newnode;
             }
@@ -71,4 +102,67 @@ public class InsertCSLL {
         }
     }
 
+    public void display(){
+        Node temp = head;
+        if (head==null){
+            System.out.println("List is empty");
+        }
+        else {
+            do {
+                System.out.print(temp.data+" ");
+                temp=temp.next;
+            }while (temp!=head);
+            System.out.println();
+        }
+    }
+
+
+    public static void main(String args[])
+    {
+        InsertCSLL s = new InsertCSLL();
+        Scanner input = new Scanner(System.in);
+
+        // for default
+        s.insertatBeg(33);
+        s.insertatBeg(22);
+        s.insertatBeg(11);
+
+        System.out.println("Enter a number");
+        int num = input.nextInt();
+
+
+
+
+        System.out.println("press 1 for insertion at Beginning");
+        System.out.println("press 2 for insertion at Specified Position");
+        System.out.println("Enter your choice: ");
+
+
+        int choice=input.nextInt();
+
+        switch(choice)
+        {
+            case 1:
+            {
+                s.insertatBeg(num);
+                s.display();
+                break;
+            }
+
+
+            case 2:
+            {
+                System.out.println("Enter a position ");
+                int pos = input.nextInt();
+                s.insertatPos(num,pos);
+                s.display();
+                break;
+            }
+
+            default:
+            {
+                System.out.println("Invalid Input");
+            }
+        }
+    }
 }
